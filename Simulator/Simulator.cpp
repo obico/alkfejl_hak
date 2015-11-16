b1f86b55fb9f11f47da112dbb7f84aa68e7ee6d1
dhuszti <huszti.daniel@gmail.com>
2015-11-16 11:32:07 +0100

    Az előzőleg felvázolt robot öntesztelésnek a megvalósítását elkezdtem. A robotot folyamatosan figyelemmel kísérjük, és ha hiba üti fel a fejét, akkor azt szövegesen kiírjuk a konzolra, a robotot pedig leállítjuk bárminemű kár elkerülése érdekében.

--- Simulator/Simulator.cpp
+++ Simulator/Simulator.cpp
@@ -11,22 +11,26 @@ Simulator::Simulator(int port)
 
 void Simulator::start(float intervalSec)
 {
+    // Számláló indítása
     dt = intervalSec;
-    state.setStatus(RobotState::Status::Start);
+    state.setStatus(RobotState::Status::Default);
     state.setTimestamp(0);
     state.setX(0.0F);
     state.setV(0.0F);
     state.setA(0.0F);
-  //  state.setLight(1);
+    state.setLight(0);
+    state.setError(0.0F);
     timer.start((long)(intervalSec*1000.0F));
 }
 
-/*void Simulator::testSimulatorFunctions()
-{
-*/
 
 void Simulator::tick()
 {
+    // Fizikai paraméterek szimulációja (legyen mindegyik 10 bit)
+    float Current = std::rand() % 1024;
+    float Voltage = std::rand() % 1024;
+
+
     // Fizikai szimuláció
     state.setTimestamp(state.timestamp() + dt);
     state.setX(state.x() + state.v()*dt);
@@ -46,26 +50,35 @@ void Simulator::tick()
     // Magasabb szintű funkciók
     switch(state.status())
     {
-    case RobotState::Status::Start:
+/*    case RobotState::Status::Start:
         state.setLight(1);                                          //lámpa induláskor égjen
-        if (1 == 0)                                           //ha nincs hiba, akkor számoljon tovább (default állapot)
+        if (0 == 0)                                                 //ha nincs hiba, akkor számoljon tovább (default állapot)
              state.setStatus(RobotState::Status::Default);
         else{                                                       //ha hiba van, akkor timer stop és resetre induljon csak újra  (reset állapot)
-            timer.stop();
+
             state.setStatus(RobotState::Status::Reset);
         }
         break;
+*/
     case RobotState::Status::Default:
+        // ide kell a folyamatos paraméter ellenőrzése, ha bármi hiba fellép akkor írja ki és állítsa le a robotszimulátort
+        if (Current > 900)
+            state.setStatus(RobotState::Status::Error);
+        if (Voltage > 900)
+            state.setStatus(RobotState::Status::Error);
+        break;
+    case RobotState::Status::Error:
+        state.setLight(1);
+        timer.stop();
         break;
     case RobotState::Status::Reset:
         qDebug() << "Simulator: Reset";
-        state.setStatus(RobotState::Status::Start);                 //reset hatására start állapotba megy és kezdi a kiértékelést újra
+        state.setStatus(RobotState::Status::Default);                 //reset hatására start állapotba megy és kezdi a kiértékelést újra
         state.setX(0.0F);
         state.setV(0.0F);
         state.setA(0.0F);
+        state.setError(0.0F);
         state.setLight(0);
-        //ide kellene a timer újraindítás
-        timer.start(1);
         break;
     case RobotState::Status::Stopping:
         if (state.v() > 1.5F)
@@ -109,7 +122,8 @@ void Simulator::tick()
              << ", x=" << state.x()
              << ", v=" << state.v()
              << ", a=" << state.a()
-             << ", lámpa:" << state.light();
+             << ", lámpa:" << state.light()
+             << ", hiba:" << state.error();
 
     // Állapot küldése
     if (communication.isConnected())
