23fe696ff42a10e127b6195b176ec76a48a370f2
Peter Almasi <peti@almasi.hu>
2015-11-21 22:57:20 +0100

    Kijavítottam az előző commitot.

--- alkfejl_hakTest/alkfejl_hakapplicationtest.cpp
+++ alkfejl_hakTest/alkfejl_hakapplicationtest.cpp
@@ -1,6 +1,6 @@
 #include "alkfejl_hakapplicationtest.h"
 
-alkfejl_hakApplicationTest::alkfejl_hakApplicationTest()
+alkfejl_hakApplicationTest::alkfejl_hakApplicationTest(QObject *parent)
 {
 
 }
--- alkfejl_hakTest/alkfejl_hakapplicationtest.h
+++ alkfejl_hakTest/alkfejl_hakapplicationtest.h
@@ -3,14 +3,18 @@
 
 #include <QObject>
 
-class alkfejl_hakApplicationTest
+class alkfejl_hakApplicationTest : public QObject
 {
+    Q_OBJECT
 public:
-    alkfejl_hakApplicationTest();
+    alkfejl_hakApplicationTest(QObject *parent = 0);
 
 signals:
 
 public slots:
+
+private slots:
+    //void myTestValuesTest();
 };
 
 #endif // ALKFEJL_HAKAPPLICATIONTEST_H
