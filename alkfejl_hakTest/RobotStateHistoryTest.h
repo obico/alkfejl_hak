0e779a1282d89d7b7e472cf1da713b9dcbc85ff8
dhuszti <huszti.daniel@gmail.com>
2015-12-07 20:51:46 +0100

    Update RobotStateHistoryTest.h

--- alkfejl_hakTest/RobotStateHistoryTest.h
+++ alkfejl_hakTest/RobotStateHistoryTest.h
@@ -8,7 +8,7 @@
 /**
  * @brief The alkfejl_hakApplicationTest class
  * Át kellett rendezni a projekt hierarchiát, hogy a unit testeknek megfelelő környezetet alakítsunk ki.
- * @see Ide és a alkfejl_hakLib-be kerülnek a unit testekhez szükséges sorok.
+ * @note Ide és a alkfejl_hakLib-be kerülnek a unit testekhez szükséges sorok.
  *
  */
 
@@ -17,7 +17,7 @@ class RobotStateHistoryTest : public RobotStateHistory
     Q_OBJECT
 
 /**
- * @see Fontos dokumentáció:Qt Creatorban unit teszt projektet létrehozva kapunk egyből egy kis keretprogramot is, ami elég jól megmutatja, hogyan is kell tesztet készíteni. A unit tesztelés általában azt jelenti, hogy a tesztelendő osztályokat példányosítjuk és mindenféle bemenetekkel bombázzuk, hogy utána ellenőrizzük a reakcióikat. A tesztelendő osztályok általában másik projektből származnak: általában több projektünk van, amiből az egyik a unit tesztek helye, és természetesen látják a többi projekt (pl. library és futtatható alkalmazás külön projektben) osztályait.
+ * @note Fontos dokumentáció:Qt Creatorban unit teszt projektet létrehozva kapunk egyből egy kis keretprogramot is, ami elég jól megmutatja, hogyan is kell tesztet készíteni. A unit tesztelés általában azt jelenti, hogy a tesztelendő osztályokat példányosítjuk és mindenféle bemenetekkel bombázzuk, hogy utána ellenőrizzük a reakcióikat. A tesztelendő osztályok általában másik projektből származnak: általában több projektünk van, amiből az egyik a unit tesztek helye, és természetesen látják a többi projekt (pl. library és futtatható alkalmazás külön projektben) osztályait.
  */
 public:
     RobotStateHistoryTest();
@@ -25,13 +25,13 @@ public:
     /* Adding a set method so we can test the getRobotState() method */
     void RobotStateTest(RobotState *currentST);
 
-    /* historyHasChanged() függvény tesztelése */
+    /* historyHasChanged() függvény tesztelése - RobotStateHistory.cpp-ben található */
     int historyHasChangedTest(const RobotState& state);
 
-    /* */
+    /* clearScreen() függvény tesztelése - RobotStateHistory.cpp-ben található */
     int clearScreenTest(const RobotState& state);
 
-
+    /* visualize() függvény tesztelése - RobotStateHistory.cpp-ben található */
     int visualizeTest(const RobotState& state);
 };
 
