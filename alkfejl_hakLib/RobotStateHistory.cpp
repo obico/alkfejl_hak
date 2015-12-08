175d4403cc67e535c7d3ee134427c4a03cfb9a39
dhuszti <huszti.daniel@gmail.com>
2015-12-08 23:30:10 +0100

    Update RobotStateHistory.cpp
    Weak pointer és shared pointer implementálása.

--- alkfejl_hakLib/RobotStateHistory.cpp
+++ alkfejl_hakLib/RobotStateHistory.cpp
@@ -24,19 +24,40 @@ void RobotStateHistory::Add(const RobotState& state)
     //  ezekből olvassák ki az adatokat, mivel erre a fenti container nem alkalmas. (Az std::vector
     //  nem támogatja a Qt metaobject rendszerét.)
     clearScreen();
+    /** @note Weak pointer és ahhoz shared pointer felhasználása
+    *   Megvizsgálom, hogy van-e egyáltalán hiba a robot működése során. Amennyiben nincs akkor a statisztikákat kirajzoltatom, kiirattatom.
+    */
 
-    int graphStateNumber = visualize();
-    //int graphStateNumber = stateList.size() < shownStateNumber ? stateList.size() : shownStateNumber;
-    auto it = container.end()-graphStateNumber;
-    for(;it!=container.end();++it)
+    std::shared_ptr<RobotState> sptr;
+
+    sptr.reset( new RobotState );
+    sptr->CopyFrom(state);
+
+    std::weak_ptr<RobotState> weak2 = sptr;
+    // weak pointer lefoglalása
+    if ( auto tmp = weak2.lock() )
     {
+        // robot esetleges hibájának kinyerése, ha "nincs hiba" akkor mindent rajzoljon ki
+        if (tmp->error() == "nincs hiba")
+        {
+            int graphStateNumber = visualize();
+            //int graphStateNumber = stateList.size() < shownStateNumber ? stateList.size() : shownStateNumber;
+            auto it = container.end()-graphStateNumber;
+            for(;it!=container.end();++it)
+            {
 
-        RobotState *currentState = it->get();
-        graphTimestamps.append(currentState->timestamp());
-        graphGyro.append(currentState->g());
-        graphVelocities.append(currentState->v());
-        graphAcceleration.append(currentState->a());
+                RobotState *currentState = it->get();
+                graphTimestamps.append(currentState->timestamp());
+                graphGyro.append(currentState->g());
+                graphVelocities.append(currentState->v());
+                graphAcceleration.append(currentState->a());
 
+            }
+        }
+    }
+    else
+    {
+        qDebug() << "weak pointer lejárt, az eredményt nem ábrázoljuk";
     }
 
     // Jelezzük a history változását.
